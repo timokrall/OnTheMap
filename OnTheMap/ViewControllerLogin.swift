6aae96649da0b8cca8aae3784074920a0652e0d6
timokrall <timo.krall@whu.edu>
2015-11-28 17:18:48 -0500

    Add outlets for login controller

--- OnTheMap/ViewControllerLogin.swift
+++ OnTheMap/ViewControllerLogin.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewControllerLogin: UIViewController {
     
+    // MARK: Outlets
+    
+    @IBOutlet weak var buttonSignInFacebook: UIButton!
+    @IBOutlet weak var buttonSignInUdacity: UIButton!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var labelLoginInformation: UILabel!
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,17 @@ class ViewControllerLogin: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Actions
+    
+    @IBAction func loginActionUdacity(sender: AnyObject) {
+    }
 
+    
+    @IBAction func loginActionFacebook(sender: AnyObject) {
+    }
+    
+    // MARK: Functions
+    
+    
+    
 }
-
