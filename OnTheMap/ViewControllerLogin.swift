387ea25a17dcccc79c243e22aff40f971ef0b86c
timokrall <timo.krall@whu.edu>
2015-11-28 17:11:11 -0500

    Storyboard for login screen added

--- OnTheMap/ViewControllerLogin.swift
+++ OnTheMap/ViewControllerLogin.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewControllerLogin: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
